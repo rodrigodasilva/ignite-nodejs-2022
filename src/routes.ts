cedecb9e96abb65bb7c17e08b6b45f2c7a9ba235
Rodrigo Ribeiro <ribeiro.rodrigodasilva@gmail.com>
2022-11-04 11:31:16 -0300

    Criar autenticacao do deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController'
+import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController'
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController'
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/CreateDeliverymanController'
 
@@ -8,9 +9,12 @@ const routes = Router()
 const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
+const authenticateDeliverymanController = new AuthenticateDeliverymanController()
+
+routes.post('/client/authenticate', authenticateClientController.handle)
+routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
 
 routes.post('/client', createClientController.handle)
-routes.post('/authenticate', authenticateClientController.handle)
 routes.post('/deliveryman', createDeliverymanController.handle)
 
 export { routes }
