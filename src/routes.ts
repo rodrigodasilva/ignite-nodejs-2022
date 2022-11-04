73d6d7f73be53c9200f2452846dda97df3c3f1f8
Rodrigo Ribeiro <ribeiro.rodrigodasilva@gmail.com>
2022-11-04 12:46:41 -0300

    Update de entrega adicionando entregador

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { AuthenticateDeliverymanController } from './modules/account/authenticat
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController'
 import { CreateDeliveryController } from './modules/deliveries/useCases /createDelivery/CreateDeliveryController'
 import { FindAllAvailableController } from './modules/deliveries/useCases /findAllAvailable/FindAllAvailableController'
+import { UpdateDeliverymanController } from './modules/deliveries/useCases /updateDeliveryman/UpdateDeliverymanController'
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/CreateDeliverymanController'
 
 const routes = Router()
@@ -16,6 +17,7 @@ const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 const createDeliveryController = new CreateDeliveryController()
 const findAllAvailableController = new FindAllAvailableController()
+const updateDeliverymanController = new UpdateDeliverymanController()
 
 routes.post('/client/authenticate', authenticateClientController.handle)
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
@@ -25,5 +27,6 @@ routes.post('/deliveryman', createDeliverymanController.handle)
 
 routes.post('/delivery', ensureAuthenticateClient, createDeliveryController.handle)
 routes.get('/delivery/available', ensureAuthenticateDeliveryman, findAllAvailableController.handle)
+routes.put('/delivery/updateDeliveryman/:id', ensureAuthenticateDeliveryman, updateDeliverymanController.handle)
 
 export { routes }
